a86cb5cf468d6200b9fb526e8b4169d14368cf95
Keith Moravec <kjemdg@yahoo.com>
2021-10-29 19:12:11 -0500

    Pybank working with print to screen - need to print to output file next

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,11 @@ import csv
 
 total_months=0
 total_profit=0
-#last_month_profit=0
 total_change_profit=0
+max_increase_profit=0
+max_decrease_profit=0
 monthly_profit_list = []
+month_list = []
 
 
 print(" ")
@@ -29,36 +31,35 @@ with open(csvpath) as csvfile:
         #print(row[1])
         
         monthly_profit_list.append(int(row[1]))
+        month_list.append(str(row[0]))
 
-        monthly_profit=int(row[1])
-        """ if row == 1:
-            last_month_profit=monthly_profit
-        else:
-            change_profit=monthly_profit-last_month_profit
-            last_month_profit=monthly_profit
-            total_change_profit=total_change_profit+change_profit    """   
+    total_months=int(len(monthly_profit_list))
+    last_month_profit=float(monthly_profit_list[0])
+    total_profit=last_month_profit
+
+    # Use monthly profit list to calculate desired financial information
+
+    for i in range(1,total_months):
+        monthly_profit=float(monthly_profit_list[i])
+        month=str(month_list[i])
         total_profit=total_profit+monthly_profit
-        total_months=total_months+1
-        
-
-    total_months_alt=len(monthly_profit_list)
-    last_month_profit=int(monthly_profit_list[0])
-
-    # Lets try this again with index we just created in block above
-    # In hindsight - probably easier to calculate change in profit with list and i, i-1....  / Change Later
-    for i in range(1,total_months_alt):
-        # print(monthly_profit_list[i])
-        monthly_profit=int(monthly_profit_list[i])
         change_profit=monthly_profit-last_month_profit
-        last_month_profit=int(monthly_profit_list[i])
+        last_month_profit=float(monthly_profit_list[i])
         total_change_profit=total_change_profit+change_profit  
+        if change_profit > max_increase_profit:
+            max_increase_profit_month=month
+            max_increase_profit = change_profit
+        if change_profit < max_decrease_profit:
+            max_decrease_profit_month=month
+            max_decrease_profit = change_profit
 
     ave_change_profit=total_change_profit/(total_months-1)
-    
-    print(f"Total Months = {total_months}")
-    print(f"Total Months = {total_months_alt}")
-    print(f"Total Profit = {total_profit}")
-    print(f"Average Change in Profit = {ave_change_profit}")
 
+    print(f"Total Months: {total_months}")
+    print(f"Total Profit: $ {total_profit:.0f}")
+    print(f"Average Change in Profit: $ {ave_change_profit:.2f}")
+    print(f"Greatest Increase in Profit: {max_increase_profit_month} (${max_increase_profit:.0f})")
+    print(f"Greatest Decrease in Profit: {max_decrease_profit_month} (${max_decrease_profit:.0f})")
+    print(" ")
 
 
